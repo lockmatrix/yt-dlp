0b3f9bb8c4839e537b0afefe3ba5ee96b684e86f
locke <hamannsun@gmail.com>
2022-07-25 10:06:45 +0800

    my selenium

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -198,6 +198,7 @@ from .bilibili import (
     BiliIntlIE,
     BiliIntlSeriesIE,
     BiliLiveIE,
+    BilibiliCheeseIE,
 )
 from .biobiochiletv import BioBioChileTVIE
 from .bitchute import (
@@ -2168,6 +2169,7 @@ from .xiami import (
     XiamiArtistIE,
     XiamiCollectionIE
 )
+from .xiaoeknow import XiaoeknownIE
 from .ximalaya import (
     XimalayaIE,
     XimalayaAlbumIE
@@ -2203,6 +2205,7 @@ from .yandexvideo import (
 )
 from .yapfiles import YapFilesIE
 from .yesjapan import YesJapanIE
+from .yhdmp import YhdmpIE
 from .yinyuetai import YinYueTaiIE
 from .ynet import YnetIE
 from .youjizz import YouJizzIE
