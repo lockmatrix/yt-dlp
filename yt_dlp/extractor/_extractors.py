c4d062a6878d698ef65b1852af6b6124570b9b7d
locke <hamannsun@gmail.com>
2022-10-27 09:49:35 +0800

    add selenium

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -222,6 +222,7 @@ from .bilibili import (
     BiliIntlIE,
     BiliIntlSeriesIE,
     BiliLiveIE,
+    BilibiliCheeseIE,
 )
 from .biobiochiletv import BioBioChileTVIE
 from .bitchute import (
@@ -1640,6 +1641,7 @@ from .saitosan import SaitosanIE
 from .samplefocus import SampleFocusIE
 from .sapo import SapoIE
 from .savefrom import SaveFromIE
+from .kudm import SbdmIE, GqdmIE
 from .sbs import SBSIE
 from .screen9 import Screen9IE
 from .screencast import ScreencastIE
@@ -2326,6 +2328,7 @@ from .yandexvideo import (
 )
 from .yapfiles import YapFilesIE
 from .yesjapan import YesJapanIE
+from .yhdmp import YhdmpIE, NtyouIE
 from .yinyuetai import YinYueTaiIE
 from .yle_areena import YleAreenaIE
 from .ynet import YnetIE
