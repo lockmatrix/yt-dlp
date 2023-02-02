19529db4ccb9937db4ff76cf721a27fed4817d3f
locke <hamannsun@gmail.com>
2023-02-02 09:46:40 +0800

    git statuscustom

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -260,6 +260,7 @@ from .bilibili import (
     BiliIntlIE,
     BiliIntlSeriesIE,
     BiliLiveIE,
+    BilibiliCheeseIE,
 )
 from .biobiochiletv import BioBioChileTVIE
 from .bitchute import (
@@ -2469,6 +2470,7 @@ from .xhamster import (
     XHamsterUserIE,
 )
 from .xiaohongshu import XiaoHongShuIE
+from .xiaoyuzhou import XiaoYuZhouIE
 from .ximalaya import (
     XimalayaAlbumIE,
     XimalayaIE,
