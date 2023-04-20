a4ebd2319c4d988acb8b1e03606d4a6b8fad135c
locke <hamannsun@gmail.com>
2023-04-20 16:34:12 +0800

    add selenium, hlsnative_fake_header

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -241,6 +241,7 @@ from .bilibili import (
     BiliIntlIE,
     BiliIntlSeriesIE,
     BiliLiveIE,
+    BilibiliCheeseIE,
 )
 from .biobiochiletv import BioBioChileTVIE
 from .bitchute import (
@@ -1738,6 +1739,7 @@ from .saitosan import SaitosanIE
 from .samplefocus import SampleFocusIE
 from .sapo import SapoIE
 from .savefrom import SaveFromIE
+from .kudm import SbdmIE, GqdmIE
 from .sbs import SBSIE
 from .sbscokr import (
     SBSCoKrIE,
